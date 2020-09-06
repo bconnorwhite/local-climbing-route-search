5dc48c5f767b1179216a6b0710917ba24be6ddc9
Wesley White <Wesley.white35@gmail.com>
2020-09-06 14:12:59 -0500

    configuring criteria

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           console.log("Latitude is :", position.coords.latitude);
           console.log("Longitude is :", position.coords.longitude);
           /*
-          fetch(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${position.coords.latitude}&lon=${position.coords.longitude}&maxDistance=25&minDiff=V5&maxDiff=V12&key=`)
+          fetch(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${position.coords.latitude}&lon=${position.coords.longitude}&maxDistance=${radius}&minDiff=${minGrade}&maxDiff=${maxGrade}&key=`)
             .then(res => res.json())
             .then((data) => {
               console.log(data)
@@ -41,15 +41,39 @@ function App() {
   },[setCurrentLocation])
   console.log(closeRoutes)
 
-  let options = [
-    {name: 'Koala'},
-    {name: 'Kangaroo'},
-    {name: 'Platypus'},
-    {name: 'Bald Eagle'},
-    {name: 'Bison'},
-    {name: 'Skunk'}
+  const [radius, setRadius] = useState(50)
+  const [numProblems, setNumProblems] = useState(50)
+
+  let gradeOptions = [
+    {name: 'V0'},
+    {name: 'V1'},
+    {name: 'V2'},
+    {name: 'V3'},
+    {name: 'V4'},
+    {name: 'V5'},
+    {name: 'V6'},
+    {name: 'V7'},
+    {name: 'V8'},
+    {name: 'V9'},
+    {name: 'V10'},
+    {name: 'V11'},
+    {name: 'V12'},
+    {name: 'V13'},
+    {name: 'V14'},
+    {name: 'V15'},
+    {name: 'V16'},
+    {name: 'V17'},
   ]
-  let [radius, setRadius] = useState('Bison')
+  let [minGrade, setMinGrade] = useState('V0')
+  let [maxGrade, setMaxGrade] = useState('V16')
+
+  useEffect(() => {
+    const selectedMinIdx = gradeOptions.findIndex(({name}) => name === minGrade)
+    const selectedMaxIdx = gradeOptions.findIndex(({name}) => name === maxGrade)
+    if (selectedMinIdx > selectedMaxIdx) {
+      setMaxGrade(gradeOptions[selectedMinIdx].name)
+    }
+  },[minGrade, maxGrade, gradeOptions])
 
 
   return (
@@ -59,7 +83,17 @@ function App() {
          `These routes are close to ${currentLocation.lat}, ${currentLocation.lon} !` :
           'Finding your location...'}
       </div>
-      <CriteriaBar radiusOptions={options} radius={radius} setRadius={setRadius}/>
+      <CriteriaBar 
+        gradeOptions={gradeOptions} 
+        minGrade={minGrade} 
+        setMinGrade={setMinGrade} 
+        maxGrade={maxGrade} 
+        setMaxGrade={setMaxGrade}
+        radius={radius}
+        setRadius={setRadius}
+        numProblems={numProblems}
+        setNumProblems={setNumProblems}
+      />
       <div className="grid grid-cols-3 gap-4">
         {closeRoutes.map(route => {
           const validImgPath = route.imgMedium.length > 0 ? route.imgMedium : route.imgSmallMed.length > 0 ? route.imgSmallMed : route.imgSmall.length > 0 ? route.imgSmall : route.imgSqSmall.length > 0 ? route.imgSqSmall : null
@@ -67,7 +101,7 @@ function App() {
           return (
             <div className="text-blue-500 border border-gray-300">
               <div className="h-40 overflow-hidden flex items-center justify-center bg-gray-200 border border-gray-100">
-                {validImgPath ? <img src={validImgPath}/> : 'No image :('}
+                {validImgPath ? <img src={validImgPath} alt="Climbing Route"/> : 'No image :('}
               </div>
               {`${route.name} - ${route.location[2]}`}
               <div className="flex justify-evenly">
--- src/CriteriaBar.tsx
+++ src/CriteriaBar.tsx
@@ -6,35 +6,53 @@ type OptionType = {
     name: string
 }
 type CriteriaPropTypes = {
-    radiusOptions: OptionType[],
-    radius: string,
-    setRadius: (radius: string) => {}
+    gradeOptions: OptionType[],
+    minGrade: string,
+    setMinGrade: (minGrade: string) => {}
+    maxGrade: string,
+    setMaxGrade: (maxGrade: string) => {}
+    radius: number,
+    setRadius: (radius: number) => {}
+    numProblems: number
+    setNumProblems: (numProblems: number) => {}
 }
 
 const CriteriaBar = (props: CriteriaPropTypes) => {
-    const {radiusOptions, radius, setRadius} = props
+    const {gradeOptions, minGrade, setMinGrade, maxGrade, setMaxGrade, radius, setRadius, numProblems, setNumProblems} = props
     return (
     <div className="justify-evenly m-auto">
         <Provider theme={defaultTheme}>
             <div>
                 <label htmlFor="radius">Radius (Miles)</label>
                 <input type="range" id="radius" name="radius"
-                        min="1" max="200"/>
+                        min="1" max="200"
+                        onChange={(selected) => setRadius(Number(selected.target.value))}
+                />
+                <span>{radius}</span>
+            </div>
+            <div>
+                <label htmlFor="numProblems">Number of problems</label>
+                <input type="range" id="numProblems" name="numProblems" value={numProblems}
+                        min="1" max="500"
+                        onChange={(selected) => setNumProblems(Number(selected.target.value))}
+                />
+                <span>{numProblems}</span>
             </div>
             <Picker
             label="Min Grade"
-            items={radiusOptions}
-            selectedKey={radius}
-            onSelectionChange={(selected) => setRadius(selected)}>
+            items={gradeOptions}
+            selectedKey={minGrade}
+            onSelectionChange={(selected) => setMinGrade(selected)}>
             {(item) => { return item && item.name ? <Item key={item.name}>{item.name}</Item> : null}}
             </Picker>
             <Picker
             label="Max Grade"
-            items={radiusOptions}
-            selectedKey={radius}
-            onSelectionChange={(selected) => setRadius(selected)}>
+            items={gradeOptions}
+            selectedKey={maxGrade}
+            onSelectionChange={(selected) => setMaxGrade(selected)}>
             {(item) => { return item && item.name ? <Item key={item.name}>{item.name}</Item> : null}}
             </Picker>
+            <button>Search</button>
         </Provider>
     </div>
 )}
